94c0430a83ec4a50fe63b2261eff094c154e0ff2
SafwanSa <safwan9f@gmail.com>
2021-03-26 00:58:56 +0300

    Some changes

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'twitter-clone',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
