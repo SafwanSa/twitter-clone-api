213f7426a1a73b76d8e49af2464c47c3f3b752b7
Safwan <safwan9f@gmail.com>
2021-03-26 03:10:13 +0300

    Add default date

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class Tweet(models.Model):
   tweet = models.TextField()
-  created_at = models.DateField()
+  created_at = models.DateField(default=timezone.now())
