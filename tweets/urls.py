668237bd756bccc2c6ab44b3d69bb632faf0ed4f
Safwan <safwan9f@gmail.com>
2021-03-26 03:18:56 +0300

    Create Tweet viewset

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .views import TweetViewSet
+from rest_framework import routers
 
+# router = routers
 urlpatterns = [
     path(''),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from rest_framework import routers
+from rest_framework import viewsets
+from .serializers import TweetSerializer
+from .models import Tweet
 # Create your views here.
+
+class TweetViewSet(viewsets.ModelViewSet):
+  queryset = Tweet.objects.all().order_by('created_at')
+  serializer = TweetSerializer
 
 
