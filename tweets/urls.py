6dd71b672131689e7d33a3132bc7b1c77e3c3d4b
SafwanSa <safwan9f@gmail.com>
2021-03-28 09:13:16 +0300

    Add manually GET endpoint

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import TweetViewSet
+from .views import TweetViewSet, list_tweets
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -7,5 +7,6 @@ router.register('tweets', TweetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tweets-list', list_tweets),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
 from .serializers import TweetSerializer
 from .models import Tweet
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 class TweetViewSet(viewsets.ModelViewSet):
-  queryset = Tweet.objects.all().order_by('created_at')
-  serializer_class = TweetSerializer
+    queryset = Tweet.objects.all().order_by('created_at')
+    serializer_class = TweetSerializer
 
 
+@api_view(['GET'])
+def list_tweets(Request):
+    tweets_list = Tweet.objects.all().order_by('created_at')
+    serializer = TweetSerializer(tweets_list, many=True)
+    return Response(serializer.data)
