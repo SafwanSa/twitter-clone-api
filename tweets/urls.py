b0b63123d10126d9e40cd792916f681941200b59
SafwanSa <safwan9f@gmail.com>
2021-03-28 09:43:08 +0300

    Create another endpoint

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import TweetViewSet, list_tweets
+from .views import TweetViewSet, list_tweets, create_tweet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -8,5 +8,6 @@ router.register('tweets', TweetViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('tweets-list', list_tweets),
+    path('tweets-create', create_tweet),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -11,7 +11,15 @@ class TweetViewSet(viewsets.ModelViewSet):
 
 
 @api_view(['GET'])
-def list_tweets(Request):
+def list_tweets(request):
     tweets_list = Tweet.objects.all().order_by('created_at')
     serializer = TweetSerializer(tweets_list, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_tweet(request):
+    data = request.body
+    serializer = TweetSerializer(data)
+    print(serializer.data)
     return Response(serializer.data)
