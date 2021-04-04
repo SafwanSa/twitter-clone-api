f5d0e8984bb46cafadd7f7b268360c80085af310
Safwan <safwan9f@gmail.com>
2021-04-05 01:31:27 +0300

    Add the obtain_auth_token to the urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register('tweets', TweetViewSet)
@@ -11,5 +12,6 @@ urlpatterns = [
     path('tweets-create', create_tweet),
     path('tweets/<pk>', get_tweet),
     path('tweets-like/<pk>', like_post),
+    path('auth-token', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
